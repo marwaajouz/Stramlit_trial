e0dfbd6ca6aabaae2e0cc2787e6b5647f9e4ccd6
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-06-04 00:38:56 +0300

    Update Trial (3).py

--- Trial (3).py
+++ Trial (3).py
@@ -1,26 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-
-
-
-# In[4]:
-
-
 import streamlit as st
 
-
-# In[6]:
-
-
 st.write("Hello ,let's learn how to build a streamlit app together")
-
-
-# In[10]:
-
 
 
 
