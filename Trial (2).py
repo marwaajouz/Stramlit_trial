0d7e67265ac31c9f78815573016bf8fb860d09dd
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-06-03 23:49:56 +0300

    Add files via upload

---
+++ Trial (2).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pip install streamlit
+
+
+# In[4]:
+
+
+import streamlit as st
+
+
+# In[6]:
+
+
+st.write("Hello ,let's learn how to build a streamlit app together")
+
+
+# In[10]:
+
+
+
+
