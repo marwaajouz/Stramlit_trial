fce8aa4a91658ac75060e6e8c34dfe9546dcc838
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-06-04 14:18:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 # app.py
 import streamlit as st
-
+st.set_page_config(
+    page_title="Coviz: COVID19 Tracking Application",
+    page_icon= "✅",
+    layout='wide'
+)
 st.write("Hello, world!")
 
