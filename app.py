dfaa9826b3b6a8af95f5be0c0fa74336e67105b9
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-06-11 14:09:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,112 +12,119 @@ st.title('Cancer in the US')
 data_path= 'https://raw.githubusercontent.com/marwaajouz/Stramlit_trial/main/General_without_regoin.csv'
 data = pd.read_csv(data_path)
 
+# Page Navigation
+pages = {
+    "Macro Overview": "macro",
+    "Pancreas Cancer": "pancreas"
+}
+page = st.sidebar.selectbox("Select a page", list(pages.keys()))
 
-# A dropdown select box for Cancer Types
-cancer_types = data['Leading Cancer Sites'].unique()
-selected_cancer_type = st.selectbox('Select a Cancer Type', cancer_types)
+if pages[page] == "macro":
+    # A dropdown select box for Cancer Types
+    cancer_types = data['Leading Cancer Sites'].unique()
+    selected_cancer_type = st.selectbox('Select a Cancer Type', cancer_types)
 
-# Filter the data based on the selected Cancer Type
-filtered_data = data[data['Leading Cancer Sites'] == selected_cancer_type]
+    # Filter the data based on the selected Cancer Type
+    filtered_data = data[data['Leading Cancer Sites'] == selected_cancer_type]
 
-# Filter the data for years 2010 and 2019
-data_2010 = filtered_data[filtered_data['Year'] == 2010]
-data_2019 = filtered_data[filtered_data['Year'] == 2019]
+    # Filter the data for years 2010 and 2019
+    data_2010 = filtered_data[filtered_data['Year'] == 2010]
+    data_2019 = filtered_data[filtered_data['Year'] == 2019]
 
-# Calculate the percentage increase or decrease
-percentage_change = ((data_2019['Crude Rate'].values - data_2010['Crude Rate'].values) / data_2010['Crude Rate'].values) * 100
-
-
-# A line plot of Crude Rate across years
-plt.figure(figsize=(10, 6))
-plt.plot(filtered_data['Year'], filtered_data['Crude Rate'])
-plt.xlabel('Year')
-plt.ylabel('Crude Rate')
-plt.title(f'Crude Rate of {selected_cancer_type} Across Years')
-
-# Add arrow annotation with percentage change
-for i, value in enumerate(percentage_change):
-    arrow_text = f'Percentage Increase: {value:.2f}%'
-    plt.annotate(arrow_text, xy=(2019, data_2019['Crude Rate'].values[i]), xytext=(2010, data_2010['Crude Rate'].values[i]-0.03),
-                 arrowprops=dict(facecolor='red', arrowstyle='->'), fontsize=8)
-
-st.pyplot(plt)
+    # Calculate the percentage increase or decrease
+    percentage_change = ((data_2019['Crude Rate'].values - data_2010['Crude Rate'].values) / data_2010['Crude Rate'].values) * 100
 
 
-#Heat map for death rate
-filtered_data = data[["Leading Cancer Sites", "Death Rate (within 5 years)"]]
-filtered_data = filtered_data.sort_values(by="Death Rate (within 5 years)", ascending=False)
+    # A line plot of Crude Rate across years
+    plt.figure(figsize=(10, 6))
+    plt.plot(filtered_data['Year'], filtered_data['Crude Rate'])
+    plt.xlabel('Year')
+    plt.ylabel('Crude Rate')
+    plt.title(f'Crude Rate of {selected_cancer_type} Across Years')
 
-#average_death_rate = filtered_data.groupby("Leading Cancer Sites")["Death Rate (within 5 years)"].mean()
-#filtered_data = filtered_data.sort_values(by="Death Rate (within 5 years)", ascending=False)
-pivot_table = filtered_data.pivot_table(index="Leading Cancer Sites", values="Death Rate (within 5 years)")
-pivot_table=pivot_table.sort_values(by="Death Rate (within 5 years)", ascending=False)
-plt.figure(figsize=(10, 8))
-sns.heatmap(pivot_table, cmap="YlGnBu", annot=True, fmt=".2f")
-plt.title("Average Death Rate by Cancer Type")
-plt.xlabel("Leading Cancer Sites")
-plt.ylabel("")
+    # Add arrow annotation with percentage change
+    for i, value in enumerate(percentage_change):
+        arrow_text = f'Percentage Increase: {value:.2f}%'
+        plt.annotate(arrow_text, xy=(2019, data_2019['Crude Rate'].values[i]), xytext=(2010, data_2010['Crude Rate'].values[i]-0.03),
+                     arrowprops=dict(facecolor='red', arrowstyle='->'), fontsize=8)
 
-st.pyplot(plt)
-
-
-# Calculate the percentage change for each unique cancer type
-data['Percentage Change'] = data.groupby('Leading Cancer Sites')['Crude Rate'].transform(lambda x: (x.iloc[-1] - x.iloc[0]) / x.iloc[0] * 100)
-#st.write(data)
+    st.pyplot(plt)
 
 
-data['Severity'] = data['Crude Rate'] * data['Percentage Change'] * data['Death Rate (within 5 years)']
-data_2019 = data[data['Year'] == 2019]
-sorted_data = data_2019.sort_values('Severity', ascending=False)
+    #Heat map for death rate
+    filtered_data = data[["Leading Cancer Sites", "Death Rate (within 5 years)"]]
+    filtered_data = filtered_data.sort_values(by="Death Rate (within 5 years)", ascending=False)
+
+    #average_death_rate = filtered_data.groupby("Leading Cancer Sites")["Death Rate (within 5 years)"].mean()
+    #filtered_data = filtered_data.sort_values(by="Death Rate (within 5 years)", ascending=False)
+    pivot_table = filtered_data.pivot_table(index="Leading Cancer Sites", values="Death Rate (within 5 years)")
+    pivot_table=pivot_table.sort_values(by="Death Rate (within 5 years)", ascending=False)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(pivot_table, cmap="YlGnBu", annot=True, fmt=".2f")
+    plt.title("Average Death Rate by Cancer Type")
+    plt.xlabel("Leading Cancer Sites")
+    plt.ylabel("")
+
+    st.pyplot(plt)
 
 
-plt.figure(figsize=(10, 6))
-plt.bar(sorted_data['Leading Cancer Sites'], sorted_data['Severity'])
-plt.xlabel('Cancer Type')
-plt.ylabel('Severity')
-plt.title('Severity of Cancer Types at Year 2019')
-plt.xticks(rotation=90)
-plt.tight_layout()
-st.pyplot(plt)
+    # Calculate the percentage change for each unique cancer type
+    data['Percentage Change'] = data.groupby('Leading Cancer Sites')['Crude Rate'].transform(lambda x: (x.iloc[-1] - x.iloc[0]) / x.iloc[0] * 100)
+    #st.write(data)
 
 
-plt.figure(figsize=(10, 6))
-plt.scatter(sorted_data['Leading Cancer Sites'], sorted_data['Severity'], s=sorted_data['Severity']*10)
-plt.xlabel('Cancer Type')
-plt.ylabel('Severity')
-plt.title('Severity of Cancer Types at Year 2019')
-plt.xticks(rotation=90)
-plt.tight_layout()
-st.pyplot(plt)
-
-##########
-from wordcloud import WordCloud
-
-wordcloud_data = dict(zip(sorted_data['Leading Cancer Sites'], sorted_data['Severity']))
-
-plt.figure(figsize=(10, 6))
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(wordcloud_data)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Severity of Cancer Types at Year 2019 (Word Cloud)')
-st.pyplot(plt)
+    data['Severity'] = data['Crude Rate'] * data['Percentage Change'] * data['Death Rate (within 5 years)']
+    data_2019 = data[data['Year'] == 2019]
+    sorted_data = data_2019.sort_values('Severity', ascending=False)
 
 
-###################################
-severity_values = sorted_data['Severity']
-cancer_types = sorted_data['Leading Cancer Sites']
+    plt.figure(figsize=(10, 6))
+    plt.bar(sorted_data['Leading Cancer Sites'], sorted_data['Severity'])
+    plt.xlabel('Cancer Type')
+    plt.ylabel('Severity')
+    plt.title('Severity of Cancer Types at Year 2019')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    st.pyplot(plt)
 
-# Create a scatter plot
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.scatter(cancer_types, severity_values)
 
-# Set labels and title
-ax.set_xlabel('Leading Cancer Sites')
-ax.set_ylabel('Severity')
-ax.set_title('Risk Chart')
+    plt.figure(figsize=(10, 6))
+    plt.scatter(sorted_data['Leading Cancer Sites'], sorted_data['Severity'], s=sorted_data['Severity']*10)
+    plt.xlabel('Cancer Type')
+    plt.ylabel('Severity')
+    plt.title('Severity of Cancer Types at Year 2019')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    st.pyplot(plt)
 
-# Rotate x-axis labels for better visibility
-plt.xticks(rotation=90)
+    ##########
+    from wordcloud import WordCloud
 
-# Display the plot using Streamlit
-st.pyplot(fig)
+    wordcloud_data = dict(zip(sorted_data['Leading Cancer Sites'], sorted_data['Severity']))
+
+    plt.figure(figsize=(10, 6))
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(wordcloud_data)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title('Severity of Cancer Types at Year 2019 (Word Cloud)')
+    st.pyplot(plt)
+
+
+    ###################################
+    severity_values = sorted_data['Severity']
+    cancer_types = sorted_data['Leading Cancer Sites']
+
+    # Create a scatter plot
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.scatter(cancer_types, severity_values)
+
+    # Set labels and title
+    ax.set_xlabel('Leading Cancer Sites')
+    ax.set_ylabel('Severity')
+    ax.set_title('Risk Chart')
+
+    # Rotate x-axis labels for better visibility
+    plt.xticks(rotation=90)
+
+    # Display the plot using Streamlit
+    st.pyplot(fig)
