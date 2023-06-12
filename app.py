b2d793e83b10868beb8a3108d21f3fb0eb8881b3
marwaajouz <135465693+marwaajouz@users.noreply.github.com>
2023-06-13 02:53:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -145,37 +145,21 @@ if pages[page] == "macro":
     plt.title('Severity of Cancer Types at Year 2019')
     plt.xticks(rotation=90)
     plt.tight_layout()
-    st.pyplot(plt)
+    #st.pyplot(plt)
     
-    col1, col2 = st.columns(([2.7, 1]))
+    col1, col2 = st.columns(([3, 1]))
 
     with col1:
         # Display the line plot
         st.pyplot(plt)
 
     with col2:
-        
-        # Display the results
-        #col3,col4 = st.columns(2)
-
+    
         col2.metric('Highest Severity', highest_severity_cancer)
-        
-
-        # Display the results
-        #col5,col6 = st.columns(2)
-
+   
         col2.metric('Severity Value', "{:,.0f}".format(highest_severity_value))
         
-            
-    column1, column2 = st.columns(2)
-   
-
-    column1.metric('Highest Severity', highest_severity_cancer)
-    column2.metric('Severity Value', "{:,.0f}".format(highest_severity_value))
-    
-
-
-
+        
     
 if pages[page] == "pancreas":
     st.subheader('Overview of Pancreas Cancer Situation in the US')
